843220a26b16039f7c5a3a71b2513cf371382da5
contour66 <zamir.thind@gmail.com>
2017-02-24 00:25:46 -0800

    Updated gifs and fixed on click issue for re-animation, began styling.

--- assets/javascript/buttons.js
+++ assets/javascript/buttons.js
@@ -1,6 +1,6 @@
 
 // Initial array of sports
-var sports = ["Golf", "Surfing",];
+var sports = ["German Shepard", "Pug", "Doberman", "Chihuahua", "St. Bernard", "Poodle", "Daschund", "Pitbull", "Terrier", "Bulldog", "Corgi", "Shibu Inu", "Great Dane", "Grey Hound"];
 // Function for adding sports data
 function renderButtons() {
   // Deleting the sport buttons prior to adding new sport buttons
@@ -48,73 +48,72 @@ $("#sports-view").on("click", ".sports", function() {
     method: "GET"
   }).done(function(response) {
 
-console.log(response);
+  console.log(response);
 
-// Since the image information is inside of the data key,
-// a variable named results is equal to response.data
-var results = response.data;
+  // Since the image information is inside of the data key,
+  // a variable named results is equal to response.data
+  var results = response.data;
 
-// ========================
+  for (var i = 0; i < results.length; i++) {
+    // Make a div with jQuery and store it in a variable named sportsDiv.
+    var sportsDiv= $("<div>");
+    // Make a paragraph tag with jQuery and store it in a variable named p.
+    var p = $("<p>");
+    // Set the inner text of the paragraph to the rating of the image in results[i].
+    p.text("Rating: " + results[i].rating);
+    // Make an image tag with jQuery and store it in a variable named animalImage.
+    var sportsImage = $("<img>").attr("data-state", "still");
+    //This variable sets the attribute for the img to the animated gif result path from the  Giphy API. It will be use to call later in our If else statement.
+    sportsImage.attr('data-animate', results[i].images.fixed_height.url);
+    //This variable sets the attribute for the img to the animated gif result path from the  Giphy API. It will be use to call later in our If else statement.
+    sportsImage.attr('data-still', results[i].images.fixed_height_still.url);
+    // Set the image's src to results[i]'s fixed_height.url.
+    sportsImage.attr( "src", results[i].images.fixed_height_still.url);
+    // Append the p variable to the animalDiv variable.
+    sportsDiv.append(p);
+    // Append the sportsImage variable to the sportsDiv variable.
+    sportsDiv.append(sportsImage);
+    // Prepend the sportsDiv variable to the element with an id of gifs-appear-here.
+    $("#gifs-appear-here").prepend(sportsDiv);
+  }
 
- for (var i = 0; i < results.length; i++) {
-
-// Make a div with jQuery and store it in a variable named sportsDiv.
-var sportsDiv= $("<div>");
-
-
-// Make a paragraph tag with jQuery and store it in a variable named p.
-var p = $("<p>");
-
-// Set the inner text of the paragraph to the rating of the image in results[i].
-p.text("Rating: " + results[i].rating);
-// Make an image tag with jQuery and store it in a variable named animalImage.
-var sportsImage = $("<img>").attr("data-state", "still");
-//This variable sets the attribute for the img to the animated gif result path from the  Giphy API. It will be use to call later in our If else statement.
-var animate = sportsImage.attr('data-animate', results[i].images.fixed_height.url);
-//This variable sets the attribute for the img to the animated gif result path from the  Giphy API. It will be use to call later in our If else statement.
-var still = sportsImage.attr('data-still', results[i].images.fixed_height_still.url);
-// Set the image's src to results[i]'s fixed_height.url.
-sportsImage.attr( "src", results[i].images.fixed_height_still.url);
-// Append the p variable to the animalDiv variable.
-sportsDiv.append(p);
-// Append the sportsImage variable to the sportsDiv variable.
-sportsDiv.append(sportsImage);
-// Prepend the sportsDiv variable to the element with an id of gifs-appear-here.
-$("#gifs-appear-here").prepend(sportsDiv);
-
-}
     
     $("img").on("click", function() {
     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
       var state = $(this).attr("data-state");
-      console.log("clicked");
+      // var still = $(this).attr("animate");
+      // console.log(still);
       // If the clicked image's state is still, update its src attribute to what its data-animate value is.
       // Then, set the image's data-state to animate
       // Else set src to the data-still value
       if (state === "still") {
-        console.log("this is still")
+        // console.log("this is still")
         $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", animate);
+        $(this).attr("data-state", "animate");
+        // console.log(animate);
       } 
       else {
         $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", still);
+        $(this).attr("data-state", "still");
       }
     
     });
   
   });
 
-});
 
-    // If state does not equal 'still', then update the src attribute of this
+
+     //If state does not equal 'still', then update the src attribute of this
 // image to it's data-animate value and update the data-state attribute to 'still'
-// else { 
+//    else { 
 
-//  self.attr("src", self.attr("data-still"));
-//   self.data("state", "still");
+// /self.attr("src", self.attr("data-still"));
+
+// self.data("state", "still");
 
 // }
+
+});
 
 
 
